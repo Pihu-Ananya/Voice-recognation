a5799700627182e9fd850fc9ab42e9bac8b01e6f
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-03-14 16:56:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-//https://teachablemachine.withgoogle.com/models/dgn-61RO9/
+
+function start(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/dgn-61RO9/model.json", modelready);
+
+}
+function modelready(){
+    classifier.classify(gotresult)
+}function gotresult(error,result){
+   console.log("gotresult");
+
+}
